23ea7b8d2b9cbe42542d064b0decb20a8af68614
Bry Cox <carl.cc@gmail.com>
2019-11-01 23:48:33 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.getElementById('create-task-form');
+
+  if (form.addEventListener){
+      form.addEventListener("submit", addFunction, false);
+      //Modern browsers
+ }else if(form.attachEvent){
+      form.attachEvent('onsubmit', addFunction)
+      //Old IE
+ }
+
+
+  function addFunction (event) {
+    let task = document.getElementById('tasks');
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode("Testing"));
+    task.appendChild(li);
+    event.preventDefault();
+  }
+
 });
